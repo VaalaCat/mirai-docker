dc539c1292564ed6e6c3037dbe68b15c7460c408
Miffy1030-llf <65667097+Miffy1030-llf@users.noreply.github.com>
2021-02-26 10:49:46 +0800

    change mirai-http config set in docker-compose.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,9 @@ if [ -f "/usr/src/mirai/config/Console/AutoLogin.yml" ]; then
   echo "fuckkkkkk"
 fi
 
+if [ -f "/usr/src/mirai/config/net.mamoe.mirai-api-http/setting.yml" ]; then
+  sed -i 's/port: .*/port: '"$EXPPORT"'/g' /usr/src/mirai/config/net.mamoe.mirai-api-http/setting.yml;
+  sed -i 's/authKey: .*/authKey: '"$AUTHKEY"'/g' /usr/src/mirai/config/net.mamoe.mirai-api-http/setting.yml;
+fi
+
 sh /usr/src/mirai/start-mirai-console.sh;
